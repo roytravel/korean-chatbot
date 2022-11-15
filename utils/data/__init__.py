2c9217e6dbabc2c9b0c83c9d019463930290c43d
DESKTOP-K6DD1JI\roytravel <roytravel97@gmail.com>
2022-11-15 20:41:12 +0900

    Add dataset, dataloader module

---
+++ utils/data/__init__.py
@@ -0,0 +1,14 @@
+from utils.data.dataset import (
+    Dataset,
+)
+
+from utils.data.dataloader import (
+    DataLoader,
+)
+
+__all__ = [
+    'DataLoader',
+    'Dataset',
+    ]
+
+assert __all__ == sorted(__all__)
